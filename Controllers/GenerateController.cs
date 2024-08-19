acf62e56878c7e2b28dd44587de5cf0417a21000
gluo169 <gluo@cavpower.com>
2024-08-19 10:33:52 +0930

    Update GenerateController.cs

--- Controllers/GenerateController.cs
+++ Controllers/GenerateController.cs
@@ -22,7 +22,7 @@ public class GenerateController : ControllerBase
 
         if (Url.ToLower() == "${phishingurl}")
         {
-            Url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            Url = "https://www.sharesbyte.com";
             isValid = true;
         }
 
